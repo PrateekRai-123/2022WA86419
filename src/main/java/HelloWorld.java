68d19067d82cb1bd8a09c80cd1602103a231ff03
PrateekRai-123 <2022wa86419@wilp.bits-pilani.ac.in>
2026-09-22 15:55:27 +0530

    Change output messages in HelloWorld.java
    Updated output messages in HelloWorld class.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
-
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Continuous Integration");
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Continuous Integration Lab");
+        System.out.println("Source code modified successfully!");
     }
 }
